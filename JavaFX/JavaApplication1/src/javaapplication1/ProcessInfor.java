69160ea7b74727a63aeac9a16ea3cf64b5343cf7
tuancvp125 <tuanlaa2k23@cvp.vn>
2024-05-29 15:21:25 +0700

    new

---
+++ JavaFX/JavaApplication1/src/javaapplication1/ProcessInfor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import oshi.SystemInfo;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OperatingSystem;
+
+/**
+ *
+ * @author admin
+ */
+public class ProcessInfor {
+    
+}
