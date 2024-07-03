02ee36f2559d9a91558654a19fc3353476891101
nOtZ3r0 <datlt.e15k2@gmail.com>
2024-07-03 20:20:17 +0700

    feat: add about us tab

---
+++ src/main/java/GUI/AboutUsController.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class AboutUsController {
+
+    @FXML
+    private Hyperlink hyperLink;
+
+    @FXML
+    public void initialize() {
+        // Any initialization code if needed
+    }
+
+    @FXML
+    private void hyperLinkAction(ActionEvent e) {
+        try {
+            Desktop desktop = Desktop.getDesktop();
+            if (Desktop.isDesktopSupported() && desktop.isSupported(Desktop.Action.BROWSE)) {
+                desktop.browse(new URI("https://github.com/D4tLe/Project1.Server_Info"));
+            } else {
+                System.out.println("Desktop or browse action not supported");
+            }
+        } catch (IOException | URISyntaxException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
