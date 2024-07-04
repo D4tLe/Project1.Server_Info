72c2f79affe76d4c5c751091dc4dd1d55ee3b736
nOtZ3r0 <datlt.e15k2@gmail.com>
2024-07-04 16:15:47 +0700

    chore: remove unnecessary file

--- MVJFX/testGui/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hust.soict.cybersec.testgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens hust.soict.cybersec.testgui to javafx.fxml;
-    exports hust.soict.cybersec.testgui;
-    requires com.github.oshi;
-}
